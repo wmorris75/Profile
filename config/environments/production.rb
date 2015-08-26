0de7de36765773dbe6d84a49c95b24f215af4dfd
wmorris75 <waynemorris75@gmail.com>
2015-08-26 13:16:07 -0400

    Add config.assets.debug = true in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,9 @@ Projects::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
+  #This is to add debugging tool
+  config.assest.debug = true
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
