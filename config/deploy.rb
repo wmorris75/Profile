8eb4f7f234f64526f3a29d113d6dc4fe681a2f07
wmorris75 <waynemorris75@gmail.com>
2015-08-26 11:43:48 -0400

    Added ruby version 2.2.1 to capistrano deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
+set :rbenv_ruby, '2.2.1'
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
