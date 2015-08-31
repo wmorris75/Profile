b37729f6b03d83e101dfdd9bb9145b8162a2e322
wmorris75 <waynemorris75@gmail.com>
2015-08-31 13:02:08 -0400

    change prod*.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Projects::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precomile = ['*.js', '*.css', '*.css.erb']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
