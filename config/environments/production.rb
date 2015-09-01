fa46cd5ce95c313862ab79062aabd51e15cbe1ed
wmorris75 <waynemorris75@gmail.com>
2015-08-31 21:54:56 -0400

    added precompiled assets files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Projects::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile += %w( *.js, *.css )
+  config.assets.precompile += %w( *.js *.css *jpg)
   Rails.application.config.assets.precompile += ['resume/resumeBuilder.js', 'resume/helper.js', 'bootstrap.js', 'bootstrap.mim.js', 'jQuery.js', 'custom_footer.css',
     'roll_in_animation.css', 'resume/style.css', 'resume/profile_pic.jpg']
 
